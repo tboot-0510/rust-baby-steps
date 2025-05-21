f48870b88875a40c716a5b0acaa7f47f6bbd6ee4
tboot-0510 <tboot@ebay.com>
2025-05-21 16:08:00 +0100

    chore: add todo-list exercice

---
+++ todo-list/src/main.rs
@@ -0,0 +1,36 @@
+pub use rust_projects::{help, Todo};
+use std::env;
+
+fn split_args(args: &[String]) -> Vec<String> {
+    let joined = args.join(" ");
+    joined
+        .split(',')
+        .map(|s| s.trim().to_string())
+        .filter(|s| !s.is_empty())
+        .collect()
+}
+
+fn main() {
+    let todo = Todo::new();
+
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    if args.len() <= 1 {
+        println!("run todo");
+        return;
+    }
+
+    let command = &args[2];
+    match command.as_ref() {
+        "list" => todo.list(),
+        "done" => todo.done(&args[3..]),
+        "edit" => todo.edit(&args[3..]),
+        "rm" => todo.remove(&args[3..]),
+        "raw" => todo.raw(&args[3..]),
+        "reset" => todo.reset(),
+        "sort" => todo.sort(),
+        "add" => todo.add(split_args(&args[3..])),
+        "help" | "--help" | "-h" | _ => help(),
+    }
+}
